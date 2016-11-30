afbb26b2468ceb28b428568ec5a5cfbb02a77524
Marcel Arrufat <marrufat@zinio.com>
2016-11-30 15:42:54 +0100

    Added solution for wordCount

---
+++ tour.golang.org/wordCount.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+
+	count := make(map[string]int)
+
+	for _, word := range strings.Fields(s) {
+		count[word]++
+	}
+	return count
+}
+
+func main() {
+	wc.Test(WordCount)
+}
